a407433018a099cc80ef04e53c9eafa8e9f6eeee
Tocchettog1 <tocchetto98@gmail.com>
2021-11-09 22:51:39 -0300

    Deletei os arquivos duplicados de outra pasta e adicionei a pasta e arquivos corretos da aula de JavaScript

---
+++ variaveis/index.js
@@ -0,0 +1,75 @@
+
+/* || Exercícios de interpretação de código ||
+
+
+
+1- Será impresso no console o seguinte:  
+10
+10 5
+
+2- Será impresso no console o seguinte:
+10 10 10
+
+3- horasDiarias    valorDiario
+
+
+
+  || Exercícios de escrita de código || 
+
+
+
+1:*/
+/*let nome = "Miguel"
+let idade = 23
+console.log(typeof idade, typeof nome)
+D- foi impresso o tipo de variavel undefined pois não foi dado
+nenhum valor para esta variável.
+F- Agora dando um valor para a variável foram impressos os tipos
+string e string.
+
+const nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+
+console.log("Olá", nome, ".Você tem", idade, "anos.")
+
+2-*/
+
+/*let perguntaUm = "Você já estudou programação?"
+ let perguntaDois = "Você possui algum conhecimento de JavaScript?"
+ let perguntaTres = "Você possui algum conhecimento de VSCode?"
+
+let respostaUm = prompt(perguntaUm)
+let respostaDois = prompt(perguntaDois)
+let respostaTres = prompt(perguntaTres)
+
+console.log(perguntaUm, "-", respostaUm)
+console.log(perguntaDois, "-", respostaDois)
+console.log(perguntaTres, "-", respostaTres)*/
+
+/* 3- 
+let a = 10
+let b = 25
+let c
+c = a
+a = b
+b = c
+
+console.log(a,b) */
+
+
+
+/* || Desafio ||*/
+
+
+
+/*
+let numeroUm = prompt("Digite um número")
+let numeroDois = prompt("Digite outro numero")
+const resposta1 = Number(numeroUm)
+const resposta2 = Number(numeroDois)
+
+let x = resposta1 + resposta2
+let y = resposta1 * resposta2
+
+console.log(x)
+console.log(y)*/
