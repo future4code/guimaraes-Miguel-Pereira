b99628a57089a38b8b69137b5f8c28df74cd7f7e
Tocchettog1 <tocchetto98@gmail.com>
2021-11-18 23:13:18 -0300

    Criei a pasta funcoes e dentro dela os arquivos .js e .html para iniciar os exercícios.

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,43 @@
+//  --- Exercícios de interpretação de código --
+
+
+
+//1- 
+//    function minhaFuncao(variavel) {
+//    return variavel *5
+//}
+//console.log(minhaFuncao(2))
+//console.log(minhaFuncao(10))
+
+//a. 10
+//   50
+//b. não apareceria nada pois precisa do console.log para que o restuda seja impresso no console.
+
+//2.
+//let textoDoUsuario = prompt("Insira um texto");
+
+//const outraFuncao = function(texto) {
+//    return texto.toLowerCase().includes("cenoura")}
+
+//const resposta = outraFuncao(textoDoUsuario)
+//console.log(resposta)
+
+//a. Esta função fará com que a mensagem escrita pelo usuário seja impressa no console toda em
+//letras minúsculas e diga se a string cenoura está ou não presente na frase impressa.
+//b. true
+//   true
+//   true
+
+
+
+//  --- Exercícios de escrita de código ---
+
+
+
+//1. a.
+//const infos = "Eu sou Miguel, tenho 22 anos, moro em Porto Alegre e sou estudante"
+//console.log(infos)
+
+
+//
+
