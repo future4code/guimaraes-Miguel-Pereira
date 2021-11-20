269edd3d1630d8e00e1a1f6fbb516bb685078270
Tocchettog1 <tocchetto98@gmail.com>
2021-11-19 22:32:12 -0300

    criei a pasta funções ondei colei os arquivos .js e .html e fiz os exercícios de funções.

--- modulo1/funcoes/index.js
+++ modulo1/funcoes/index.js
@@ -14,13 +14,13 @@
 //b. não apareceria nada pois precisa do console.log para que o restuda seja impresso no console.
 
 //2.
-//let textoDoUsuario = prompt("Insira um texto");
+/* let textoDoUsuario = prompt("Insira um texto");
 
-//const outraFuncao = function(texto) {
-//    return texto.toLowerCase().includes("cenoura")}
+const outraFuncao = function(texto) {
+    return texto.toLowerCase().includes("cenoura")}
 
-//const resposta = outraFuncao(textoDoUsuario)
-//console.log(resposta)
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta) */
 
 //a. Esta função fará com que a mensagem escrita pelo usuário seja impressa no console toda em
 //letras minúsculas e diga se a string cenoura está ou não presente na frase impressa.
@@ -35,9 +35,86 @@
 
 
 //1. a.
-//const infos = "Eu sou Miguel, tenho 22 anos, moro em Porto Alegre e sou estudante"
-//console.log(infos)
+/* function sobreEu(){
 
+"Eu sou Miguel, tenho 22 anos, moro em Porto Alegre e sou estudante"
 
-//
+}
+console.log(sobreEu) */
+//b.
+//function infos(nome, idade, endereco, profissao) {
+  // console.log(nome, idade, endereco, profissao)
+//   console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e sou ${profissao}.`) 
+ 
+//}
+//infos("Miguel", 22, "Porto Alegre", "empresário")
+
+//2. a.
+/* const somaDosNumeros =  (numero1 , numero2) => {
+    return  numero1 + numero2
+  }
+  
+  console.log(somaDosNumeros(4, 8)) */
+
+//b. 
+/* const compararNumeros = (numero1 , numero2) => {
+    return numero1 >= numero2
+}
+
+console.log(compararNumeros(6, 4)) */
+
+//c.
+/*const numeroPar = (numero) => {
+
+    return numero %2 ==0
+}
+
+console.log(numeroPar(48))*/
+
+//d.
+/* function mensagem(texto) {
+let maiuscula = texto.toUpperCase()
+let tamanho = texto.length
+
+return maiuscula + ". Tamanho:" + tamanho
+    
+}
+console.log(mensagem(prompt("Digite algo"))) */
+
+//3.
+/* function soma(num1, num2){
+
+    let soma = num1 + num2
+
+    return soma
+}
+
+function subtracao(num1, num2){
+
+    let subtracao = num1 - num2
+
+    return subtracao
+}
+
+function divisao(num1, num2){
+
+    let divisao = num1 / num2
+
+    return divisao
+}
+
+function multiplicacao(num1, num2){
+
+    let multiplicacao = num1 * num2 
+
+    return multiplicacao
+}
+
+let a = Number(prompt("Digite um número"))
+let b = Number(prompt("Digite outro número"))
+
+console.log(soma(a,b))
+console.log(subtracao(a,b))
+console.log(multiplicacao(a,b))
+console.log(divisao(a,b)) */
 
