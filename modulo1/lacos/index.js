bf0baa3973892315e12a54d482bbd3da15cb85b0
Tocchettog1 <tocchetto98@gmail.com>
2021-11-30 22:23:29 -0300

    Criei os arquivos index.js e index.html e iniciei os exercícios de loop

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -1,1 +1,31 @@
-console.log("A")
+// ---- Exercícios de interpretação de código ----
+
+
+
+//1. Ele está inciando o comando no índice 0 e somando o valor da conta
+//com o índice até que o valor de índice seja =>5. Será impresso o resultado 10.
+
+//2. a. [19, 21, 23, 25, 27, 30]
+//b. Não. 
+
+
+
+//---- Exercícios de escrita de código ----
+
+
+
+// //1.
+// const quantosPets = Number(prompt("Quantos pets você tem?"))
+
+// let arrayNomes = []
+
+// if(quantosPets === 0) {
+//   console.log("Que pena! Você poderia adotar um pet.")
+  
+// } else if(quantosPets > 0){
+//   for(let i=0; i < quantosPets; i++){
+//     arrayNomes.push(prompt("Quais os nomes dos teus pets?"))
+// }
+// console.log("Nomes:", arrayNomes)
+
+// }
