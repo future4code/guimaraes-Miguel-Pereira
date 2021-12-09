c1f69e67bda5d1a614ec9c50bdada6e69b2998d3
Tocchettog1 <tocchetto98@gmail.com>
2021-12-09 18:54:54 -0300

    Conclui os exercícios de 1 à 9

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,32 +5,48 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   
+   return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  
+  return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
   
+    return array.sort(function(a,b){return a-b})
 }
-
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    let novaArray = []
+    for(let item of array) {
+        if(item % 2 === 0) {
+            novaArray.push(item)
+        }
+    }
+    return novaArray
+
 }
+
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    let novaArray = []
+    for(let item of array) {
+        if(item % 2 === 0 ) {
+            novaArray.push(Math.pow(item, 2))
+        }
+    }
+    return novaArray
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+ return array.reduce(function(a,b){
+     return Math.max(a,b)
+ })
 }
 
 // EXERCÍCIO 07
@@ -40,12 +56,24 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let numerosPares = []
+   for( let i = 0; numerosPares.length < n; i++){
+       if (i % 2 === 0 ){
+           numerosPares.push(i)
+       }
+   }
+   return numerosPares
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+if (ladoA !== ladoB && ladoB !== ladoC && ladoC !== ladoA){
+    return 'Escaleno'
+} else if(ladoA === ladoB && ladoB=== ladoC && ladoC === ladoA){
+    return 'Equilátero'
+} {
+    return 'Isósceles'
+}
 }
 
 // EXERCÍCIO 10
