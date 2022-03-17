71d96507ae8aefec4ee170f1c7e8e6f6565479c7
Miguel Tocchetto <miguelpereira.dev@gmail.com>
2022-03-16 23:25:09 -0300

    Atividade concluída

--- modulo4/use-state/aula-hooks/insta-lab-hooks/src/components/Post/Post.js
+++ modulo4/use-state/aula-hooks/insta-lab-hooks/src/components/Post/Post.js
@@ -10,16 +10,57 @@ import iconeComentario from '../../img/comment_icon.svg'
 
 const Post = (props) => {
 
+  const [curtido, setCurtido] = useState(false);
+  const [numeroCurtidas, setNumeroCurtidas] = useState(0);
+  const [comentando, setComentando] = useState(false);
+  const [numeroComentarios, setNumeroComentarios] = useState(0);
+  const [comentarios, setComentarios] = useState([]);
+
 
   const onClickCurtida = () => {
+        // verifica se, no estado, cutido é true ou false
+        if (curtido) {
+
+          // se for true, entra nesse caso, e o estado é modificado de acordo com esses comandos:
+          setCurtido(!curtido)
+          setNumeroCurtidas(numeroCurtidas -1)
+        } else {
+    
+          // se for false, entra nesse caso, e o estado é modificado de acordo com esses comandos:
+          setCurtido(!curtido)
+          setNumeroCurtidas(numeroCurtidas +1)
+          
+        }
   };
 
   const onClickComentario = () => {
+    setComentando(!comentando)
   };
 
   const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+
+    setComentarios(listaDeComentarios);
+    setComentando(false);
+    setNumeroComentarios(numeroComentarios + 1)
   }
 
+  const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
+  
+  const caixaDeComentario = comentando ? (
+    // Componente com o input e botão de enviar novo comentario. Import dele na linha 5.
+    <SecaoComentario enviarComentario={enviarComentario}/>
+  ) : (
+    // Funcao map sendo feita na propriedade comentarios do estado. Ou seja, está sendo pego todos os
+    // comentários do estado para serem renderizados na tela, dentro do componente CommentContainer 
+    comentarios.map(comentario => {
+      return (
+        <CommentContainer>
+          <p>{comentario}</p>
+        </CommentContainer>
+      )
+    })
+  )
   return (
     <PostContainer>
       <PostHeader>
@@ -31,18 +72,18 @@ const Post = (props) => {
 
       <PostFooter>
         <IconeComContador
-          // icone={iconeCurtida}
+          icone={iconeCurtida}
           onClickIcone={onClickCurtida}
-          // valorContador={numeroCurtidas}
+          valorContador={numeroCurtidas}
         />
 
         <IconeComContador
           icone={iconeComentario}
           onClickIcone={onClickComentario}
-          // valorContador={numeroComentarios}
+          valorContador={numeroComentarios}
         />
       </PostFooter>
-      {/* {caixaDeComentario} */}
+      {caixaDeComentario}
     </PostContainer>
   )
 }
