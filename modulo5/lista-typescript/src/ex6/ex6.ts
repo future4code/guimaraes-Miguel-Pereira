441c551cd92e377edf5f2ae3d5af74745b365bf6
Miguel Tocchetto <miguelpereira@dev.com.br>
2022-05-30 00:04:45 -0300

    lista de exercícios concluída (Desafio iniciado)

--- modulo5/lista-typescript/src/ex6/ex6.ts
+++ modulo5/lista-typescript/src/ex6/ex6.ts
@@ -1,17 +1,28 @@
-function retornaContasComSaldoAtualizado(contas: number[]): number[] {
+type infoClients = {
+    client: string,
+    totalBalance: number,
+    debits: number[]
+}
 
-    let somaCompras: number = undefined;
-    for (const conta of contas) {
-        somaCompras = conta.compras.reduce((soma: number, i: number) => {
-            soma += i
-            return soma
+let clients: infoClients[] = [
+	{ client: "João", totalBalance: 1000, debits: [100, 200, 300] },
+	{ client: "Paula", totalBalance: 7500, debits: [200, 1040] },
+	{ client: "Pedro", totalBalance: 10000, debits: [5140, 6100, 100, 2000] },
+	{ client: "Luciano", totalBalance: 100, debits: [100, 200, 1700] },
+	{ client: "Artur", totalBalance: 1800, debits: [200, 300] },
+	{ client: "Soter", totalBalance: 1200, debits: [] }
+];
 
-        });
-        conta.saldoTotal -= somaCompras
+function NegativeClients(clients: infoClients[]): infoClients[] {
+        clients.forEach((client) => { 
+        const finalDebits = client.debits.reduce((a , b) => a + b, 0);
+        client.totalBalance -= finalDebits;
+        client.debits = []
+    })
+    const negativeCostumers = clients.filter((client) => client.totalBalance < 0);
 
+    return negativeCostumers;   
+ 
+};
 
-        conta.compras = []
-
-        return contas
-    }
-} 
+console.log(NegativeClients(clients));
