6702fcef452ca14b741d3f9f9f7064bbd3d5d992
Miguel Tocchetto <miguelpereira@dev.com.br>
2022-06-01 00:46:52 -0300

    Intro express (Finalizado)

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -3,29 +3,63 @@ import cors from "cors";
 import express, {Request, Response} from "express";
 import { AddressInfo } from "net";
 import { Users } from "./users";
+import { Posts } from "./posts";
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-
-//Ex 1.
-app.get("/", (req: Request, res: Response) => {          
-    res.status(200).send("Tudo ok")
-});
-
-app.post("/",)
-
-
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
+    console.log(`Servidor rodando em: http://localhost:${address.port}`);
   } else {
-    console.error(`Failure upon starting server.`);
+    console.error(`Falha na incialização do servidor.`);
   }
-});;
+});
 
+//Ex 1 a 4
+app.get("/", (req: Request, res: Response) => {          
+      res.status(200).send("Tudo ok")
+});
 
+app.get("/users", (req: Request, res: Response) => {
 
+  try{
+    res.status(200).send(Users)
+  }
+  catch{
+    if(!Users.length){
+      res.status(400).send("falha na requisição")
+    }
+  }
+  
+})
 
+//Ex 5 a 8
+// Criei o Array de posts em um novo arquivo para 
+//deixar o projeto melhor organizado.
+app.get("/posts", (req: Request, res: Response) => {
+  try{
+    res.status(200).send(Posts)
+  }
+  catch{
+    if(!Posts.length){
+      res.status(400).send("falha na requisição")
+    }
+  }
+})
+
+app.get("/posts/:id", (req: Request, res: Response) => {
+  try{
+    const id = Number(req.params.id)
+    const filterPost = Posts.filter((post) => {
+      if(id === post.id)
+      return post
+    })
+    res.status(200).send(filterPost);
+  }
+  catch(error){
+    res.status(400).send("Post não encontrado ou inexistente")
+  }
+})
---
+++ modulo5/intro-express/src/posts.ts
@@ -0,0 +1,63 @@
+type PostsDetails = {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+export const Posts: PostsDetails[] = [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+        "userId": 2,
+        "id": 13,
+        "title": "dolorum ut in voluptas mollitia et saepe quo animi",
+        "body": "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
+      },
+      {
+        "userId": 2,
+        "id": 14,
+        "title": "voluptatem eligendi optio",
+        "body": "fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum"
+      },
+      {
+        "userId": 2,
+        "id": 15,
+        "title": "eveniet quod temporibus",
+        "body": "reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae"
+      },
+      {
+        "userId": 3,
+        "id": 23,
+        "title": "maxime id vitae nihil numquam",
+        "body": "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis"
+      },
+      {
+        "userId": 5,
+        "id": 50,
+        "title": "repellendus qui recusandae incidunt voluptates tenetur qui omnis exercitationem",
+        "body": "error suscipit maxime adipisci consequuntur recusandae\nvoluptas eligendi et est et voluptates\nquia distinctio ab amet quaerat molestiae et vitae\nadipisci impedit sequi nesciunt quis consectetur"
+      },
+      {
+        "userId": 6,
+        "id": 51,
+        "title": "soluta aliquam aperiam consequatur illo quis voluptas",
+        "body": "sunt dolores aut doloribus\ndolore doloribus voluptates tempora et\ndoloremque et quo\ncum asperiores sit consectetur dolorem"
+      },
+      {
+        "userId": 6,
+        "id": 52,
+        "title": "qui enim et consequuntur quia animi quis voluptate quibusdam",
+        "body": "iusto est quibusdam fuga quas quaerat molestias\na enim ut sit accusamus enim\ntemporibus iusto accusantium provident architecto\nsoluta esse reprehenderit qui laborum"
+      }
+]
