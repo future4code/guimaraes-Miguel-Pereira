3a9d72334ff7be387b42f4601a861dbb40904d75
Miguel Tocchetto <miguelpereira@dev.com.br>
2022-06-03 20:20:53 -0300

    Exercícios express concluídos

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -3,26 +3,63 @@ import cors from "cors"
 import { app } from "./app"
 import { TasksList } from "./data"
 
-
+//Inicial Test
 app.get("/ping", (req: Request, res: Response) => {
     res.send("Pong! 🏓")
 })
 
-app.get("/tasks/:id", (req: Request, res: Response) => {
-    const userId = Number(req.params.id)
-    const taskCompleted = Boolean(req.query.completed)
-    const taskDone = TasksList.filter((user)=>{
-        if( userId === user.userId && taskCompleted === user.completed ){
-            return user
-        } 
-    })
-        res.status(200).send(taskDone)
-    
-    })
+//Get All Tasks
+app.get("/tasks", (req: Request, res: Response) => {
+    res.status(200).send(TasksList) 
+})
 
-app.post("/task/:id/create", (req: Request, res: Response) => {
-    const userId = Number(req.params.id)
-    const id = Math.random()
+//Get Task By Completed
+app.get("/user/:userId/tasks/:completed", (req: Request, res: Response) => {
+    const userId = Number(req.params.userId)
+    const taskCompleted: any = req.params.completed
+
+    const taskDone = TasksList.filter((task)=>{       
+        if (taskCompleted === "true" && task.completed === true && userId === task.userId) {
+            return task;
+        } else if (taskCompleted === "false" && task.completed === false && userId === task.userId) {
+            return task;
+        }
+})
+    res.status(200).send(taskDone)
+    
+})  
+
+//Get All Tasks From User
+app.get("/tasks/user/:userId", (req: Request, res: Response) => {
+    const userId = Number(req.params.userId)
+    const allTasks = TasksList.filter((user)=>{
+        if( userId === user.userId)
+        return user
+    })
+    try {
+        res.status(200).send(allTasks)
+    }
+    catch{
+        res.status(400).send("Usuário não encontrado ou inexistente")
+    }
+})
+
+//Get Task From User
+app.get("/user/:userId/task/:id", (req: Request, res: Response) => {
+    const userId = Number(req.params.userId)
+    const taskId = Number(req.params.id)
+
+    const userTask = TasksList.filter((user)=>{
+        if( userId === user.userId && taskId === user.id)
+        return user
+    })
+  res.status(200).send(userTask)
+})
+
+//Create Task
+app.post("/task/create/:userId", (req: Request, res: Response) => {
+    const userId = Number(req.params.userId)
+    const id = Math.round(Math.random() * (10 - 5) + 5)
     const title = req.body.title
     const completed = Boolean(req.body.completed)
 
@@ -37,48 +74,35 @@ app.post("/task/:id/create", (req: Request, res: Response) => {
 
 })
 
-app.put("/task/edit/:id", (req: Request, res: Response) => {
+//Edit Task
+app.put("/user/:userId/task/edit/:id", (req: Request, res: Response) => {
+    const userId = Number(req.params.userId)
+    const taskId = Number(req.params.id)
+    const completed = Boolean(req.body.completed)
 
-})
+    TasksList.forEach((user) => {
+        if(userId === user.userId && taskId === user.id){
 
-app.get("/tasks/user/:id", (req: Request, res: Response) => {
-    const id = Number(req.params.id)
-    const allTasks = TasksList.filter((user)=>{
-        if( id === user.userId)
-        return user
+            user.completed = completed;
+        }
     })
-    try {
-        res.status(200).send(allTasks)
-    }
-    catch{
-        res.status(400).send("Usuário não encontrado ou inexistente")
-    }
+
+    res.status(200).send(TasksList);
+
+
 })
 
-app.delete("/task/delete/:id", (req: Request, res: Response) => {
-    const id = Number(req.params.id)
+//Delete Task
+app.delete("/user/:userId/task/delete/:id", (req: Request, res: Response) => {
+    const userId = Number(req.params.userId)
+    const taskId = Number(req.params.id)
 
     TasksList.forEach((task, i) => {
-        if(task.id === id){
+        if(userId === task.userId && taskId === task.id){
             TasksList.splice(i, 1)
         }
+       
     })
 
     res.status(200).send(TasksList)
 } )
-
-app.get("/user/:userId/task/:id", (req: Request, res: Response) => {
-    const taskId = Number(req.params.id)
-    const userId = Number(req.params.id)
-
-    const userTask = TasksList.filter((user)=>{
-        if( userId === user.userId && taskId === user.id)
-        return user
-    })
-    try {
-        res.status(200).send(userTask)
-    }
-    catch{
-        res.status(400).send("Task não encontrada ou inexistente")
-    }
-})
