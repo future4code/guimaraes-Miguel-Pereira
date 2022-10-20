adc5c101b36edc1e5319fa9363e9219328a8afba
Miguel Tocchetto <miguelpereira@dev.com.br>
2022-10-19 23:01:35 -0300

    Save de segurança

--- cases/personare-tarot/src/App.js
+++ cases/personare-tarot/src/App.js
@@ -1,15 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
-
-
-const ComponenteDeTeste = () => {
-  return(
-    <div>
-      <p>Olá, sou um componente</p>
-    </div>
-  )
-}
+import { Card } from './components/Cards';
 
 
 function App() {
@@ -19,6 +10,21 @@ function App() {
         <Header/>
       </header>
       <body className='App-body'>
+        <Card 
+        text={"O MAGO"}
+        image={"https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano1.jpg"}/>
+        <Card 
+        text={"A SACERDOTISA"}
+        image={"https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano2.jpg"}/>
+        <Card 
+        text={"A IMPERATRIZ"}
+        image={"https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano4.jpg"}/>
+        <Card 
+        text={"O IMPERADOR"}
+        image={"https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano5.jpg"}/>
+        <Card 
+        text={"O PAPA"}
+        image={"https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano6.jpg"}/>
         <p>mensagem de teste</p>
       </body>
       <footer className='App-footer'>
--- cases/personare-tarot/src/components/Cards.js
+++ cases/personare-tarot/src/components/Cards.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default Cards = () => {
+export const Card = (props) => {
+    console.log(props)
     return(
         <div>
-            
+            <p>{props.text}</p>
+            <img src={props.image}/>
         </div>
     )
 }
